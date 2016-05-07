f00080d0300032259d0a1b5821435ebbdd8b45f1
msl12 <2496227361@qq.com>
2016-05-07 14:19:26 +0800

    1.93

--- config/initializers/file_uploader.rb
+++
@@ -1,9 +0,0 @@
-class FileUploader < CarrierWave::Uploader::Base
-
-	storage :file
-
-  	def store_dir
-    		"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  	end
-
-end
--- db/migrate/20160507061015_add_index_to_tags.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToTags < ActiveRecord::Migration
-  def change
-  	add_index(:tags, :name, unique: true)
-  end
-end
