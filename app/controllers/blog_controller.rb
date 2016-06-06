c971ef981253dd3599f84035c42163dc25410cc3
msl12 <2496227361@qq.com>
2016-06-06 23:37:19 +0800

    show my markdown

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -6,7 +6,10 @@ class BlogController < ApplicationController
 
 	def show
 		@blog = Blog.find(params[:id].to_i)
-		@blog.increment_view_count
+		 respond_to do |format|
+		 	format.md
+		 	format.html { @blog.increment_view_count }
+      		end
 	end
 
 	def tag
